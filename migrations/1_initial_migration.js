af5d0315d95a4aa34ffb75978f5a8a872c05f8e4
johni1228 <akirus1228@gmail.com>
2021-08-20 15:31:22 -0700

    fea: set baseURI

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,8 @@
 const MyLittleComputer = artifacts.require('MyLittleComputer');
+const baseURI = 'http://54.146.118.202/computer-api/v0/nfts/';
 
 module.exports = function(deployer) {
-  deployer.deploy(MyLittleComputer).then(() => {
+  deployer.deploy(MyLittleComputer, baseURI).then(() => {
     console.log('MyLittleComputer is deployed.');
   });
 };
